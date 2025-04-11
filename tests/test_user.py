45a160bc16ffcd2d3435a8054f06080ee1224017
wpyatzy <sanyasavin08@gmail.com>
2025-04-11 11:59:58 +0300

    f

---
+++ tests/test_user.py
@@ -0,0 +1,58 @@
+from fastapi.testclient import TestClient
+from src.main import app
+
+client = TestClient(app)
+
+# Существующие пользователи
+users = [
+    {
+        'id': 1,
+        'name': 'Ivan Ivanov',
+        'email': 'i.i.ivanov@mail.com',
+    },
+    {
+        'id': 2,
+        'name': 'Petr Petrov',
+        'email': 'p.p.petrov@mail.com',
+    },
+    {
+        'id': 3,
+        'name': 'Sergey Sergeev',
+        'email': 's.s.sergeev@mail.com',
+    }
+]
+
+def test_get_existed_user():
+    '''Получение существующего пользователя'''
+    response = client.get("/api/v1/user", params={'email': users[0]['email']})
+    assert response.status_code == 200
+    assert response.json() == users[0]
+
+def test_get_unexisted_user():
+    '''Получение несуществующего пользователя'''
+    response = client.get("/api/v1/user", params={'email': "nonexistent@example.com"})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
+
+def test_create_user_with_valid_email():
+    '''Создание пользователя с уникальной почтой'''
+    response = client.post("/api/v1/user", json={
+        "name": users[2]['name'],
+        "email": users[2]['email']
+    })
+    assert response.status_code == 201
+    assert isinstance(response.json(), int)
+
+def test_create_user_with_invalid_email():
+    '''Создание пользователя с почтой, которую использует другой пользователь'''
+    response = client.post("/api/v1/user", json={
+        "name": "Duplicate User",
+        "email": users[0]['email']
+    })
+    assert response.status_code == 409
+    assert response.json() == {"detail": "User with this email already exists"}
+
+def test_delete_user():
+    '''Удаление пользователя'''
+    delete_response = client.delete("/api/v1/user", params={'email': users[0]['email']})
+    assert delete_response.status_code == 204
